7a4688f6db2fae48443d559de5695c91cef81622
xxzkid <xxzkid@sina.com>
2023-06-20 14:52:51 +0000

    Update Docs by Github Actions

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,80 @@
-self.addEventListener('install', (event) => {
+var CACHE_VERSION = 2;
+var CURRENT_CACHES = {
+  prefetch: 'prefetch-cache-v' + CACHE_VERSION
+};
+
+self.addEventListener('install', function(event) {
+  var urlsToPrefetch = [
+  ];
+  // All of these logging statements should be visible via the "Inspect" interface
+  // for the relevant SW accessed via chrome://serviceworker-internals
+  console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
+  
+  self.skipWaiting();
+  
   event.waitUntil(
-    caches.open('cache_static').then((cache) => {
-      
-    }).then(() => {
-      self.skipWaiting();
+    caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // While there is only one cache in this example, the same logic will handle the case where
+  // there are multiple versioned caches.
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+        );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
   if (
     /.*\.mp3/.test(event.request.url)
   ) {
-    var request = event.request;
-    event.respondWith(firstCache(request, 'cache_music'));
+    if (event.request.headers.get('range')) {
+      var pos =
+      Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+      console.log('Range request for', event.request.url, ', starting position:', pos);
+      event.respondWith(
+        caches.open(CURRENT_CACHES.prefetch)
+        .then(function(cache) {
+          return cache.match(event.request.url);
+        }).then(function(res) {
+          if (!res) {
+            return fetch(event.request)
+            .then(res => {
+              return res.arrayBuffer();
+            });
+          }
+          return res.arrayBuffer();
+        }).then(function(ab) {
+          return new Response(
+            ab.slice(pos),
+            {
+              status: 206,
+              statusText: 'Partial Content',
+              headers: [
+                ['Content-Range', 'bytes ' + pos + '-' + (ab.byteLength - 1) + '/' + ab.byteLength]]
+            });
+        }));
+    }
+    // var request = event.request;
+    // event.respondWith(firstCache(request, 'cache_music'));
   }
 });
 
