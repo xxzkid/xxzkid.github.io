f53f2a1adb47fae2cd0efb15fae6653e454edbec
xxzkid <xxzkid@sina.com>
2023-06-20 15:25:31 +0000

    Update Docs by Github Actions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 2;
+var CACHE_VERSION = 3;
 var CURRENT_CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION
 };
@@ -43,98 +43,59 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  if (
-    /.*\.mp3/.test(event.request.url)
-  ) {
-    if (event.request.headers.get('range')) {
-      var pos = Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-      console.log('Range request for', event.request.url, ', starting position:', pos);
-      event.respondWith(
-        caches.open(CURRENT_CACHES.prefetch)
-        .then(function(cache) {
-          return cache.match(event.request.url);
-        }).then(function(res) {
-          if (!res) {
-            return fetch(event.request)
-            .then(res => {
-              return res.arrayBuffer();
-            });
-          }
-          console.log('cached : ', event.request.url);
-          document.getElementById('m-cached-text').innerHTML = event.request.url;
-          return res.arrayBuffer();
-        }).then(function(ab) {
-          return new Response(
-            ab.slice(pos),
-            {
-              status: 206,
-              statusText: 'Partial Content',
-              headers: [
-                ['Content-Range', 'bytes ' + pos + '-' + (ab.byteLength - 1) + '/' + ab.byteLength]]
-            });
-        }));
-    } else {
-      console.log('Non-range request for', event.request.url);
-      event.respondWith(
-        // caches.match() will look for a cache entry in all of the caches available to the service worker.
-        // It's an alternative to first opening a specific named cache and then matching on that.
-        caches.match(event.request).then(function(response) {
-          if (response) {
-            console.log('Found response in cache:', response);
-            return response;
-          }
-          console.log('No response found in cache. About to fetch from network...');
-          // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
-          // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
-          return fetch(event.request).then(function(response) {
-            console.log('Response from network is:', response);
-    
-            return response;
-          }).catch(function(error) {
-            // This catch() will handle exceptions thrown from the fetch() operation.
-            // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-            // It will return a normal response object that has the appropriate error code set.
-            console.error('Fetching failed:', error);
-    
-            throw error;
-          });
-        })
-      );
-    }
-  } else {
+  if (event.request.headers.get('range')) {
+    var pos = Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+    console.log('Range request for', event.request.url, ', starting position:', pos);
     event.respondWith(
+      caches.open(CURRENT_CACHES.prefetch)
+      .then(function(cache) {
+        return cache.match(event.request.url);
+      }).then(function(res) {
+        if (!res) {
+          return fetch(event.request)
+          .then(res => {
+            return res.arrayBuffer();
+          });
+        }
+        console.log('cached : ', event.request.url);
+        document.getElementById('m-cached-text').innerHTML = event.request.url;
+        return res.arrayBuffer();
+      }).then(function(ab) {
+        return new Response(
+          ab.slice(pos),
+          {
+            status: 206,
+            statusText: 'Partial Content',
+            headers: [
+              ['Content-Range', 'bytes ' + pos + '-' + (ab.byteLength - 1) + '/' + ab.byteLength]]
+          });
+      }));
+  } else {
+    console.log('Non-range request for', event.request.url);
+    event.respondWith(
+      // caches.match() will look for a cache entry in all of the caches available to the service worker.
+      // It's an alternative to first opening a specific named cache and then matching on that.
       caches.match(event.request).then(function(response) {
         if (response) {
           console.log('Found response in cache:', response);
           return response;
         }
         console.log('No response found in cache. About to fetch from network...');
+        // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+        // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
         return fetch(event.request).then(function(response) {
           console.log('Response from network is:', response);
+  
           return response;
         }).catch(function(error) {
+          // This catch() will handle exceptions thrown from the fetch() operation.
+          // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+          // It will return a normal response object that has the appropriate error code set.
           console.error('Fetching failed:', error);
+  
           throw error;
         });
       })
     );
   }
 });
-
-function firstCache(request, cacheName) {
-  return caches.match(request).then((cache) => {
-    if (cache) {
-      console.log('cached : ', request.url);
-      document.getElementById('m-cached-text').innerHTML = request.url;
-      return cache;
-    }
-    var requestCopy = request.clone();
-    return fetch(requestCopy).then((response) => {
-      var responseCopy = response.clone();
-      caches.open(cacheName).then((cache) => {
-        cache.put(requestCopy, responseCopy);
-      });
-      return response;
-    });
-  });
-}
