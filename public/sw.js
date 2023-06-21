b2f340a88b31ef87a732a7c26af60bbf8a4ccb61
xxzkid <xxzkid@sina.com>
2023-06-21 02:05:38 +0000

    Update Docs by Github Actions

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,27 @@
 importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
-workbox.setConfig({
-  modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/'
-});
-
-workbox.precaching([
-  // 注册成功后要立即缓存的资源列表
-]);
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.html'),
-  workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.(?:js|css)'),
-  workbox.strategies.cacheFirst()
-);
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.mp3'),
-  workbox.strategies.cacheFirst()
-);
+if (workbox) {
+  workbox.setConfig({
+    modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/'
+  });
+  
+  workbox.precaching([
+    // 注册成功后要立即缓存的资源列表
+  ]);
+  
+  workbox.routing.registerRoute(
+    new RegExp('.*\.html'),
+    workbox.strategies.networkFirst()
+  );
+  
+  workbox.routing.registerRoute(
+    new RegExp('.*\.(?:js|css)'),
+    workbox.strategies.cacheFirst()
+  );
+  
+  workbox.routing.registerRoute(
+    new RegExp('.*\.mp3'),
+    workbox.strategies.cacheFirst()
+  );
+} else {
+  console.log('workbox did not load');
+}
