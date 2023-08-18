7e7ebbea042659bf188547de269e4460a6b73a40
xxzkid <xxzkid@sina.com>
2023-08-18 03:30:30 +0000

    Update Docs by Github Actions

---
+++ public/log.js
@@ -0,0 +1,26 @@
+const _console_log = console.log;
+const _console_error = console.error;
+console.log = function () {
+  _console_log(...arguments);
+  _append_log(...arguments);
+}
+console.error = function () {
+  _console_error(...arguments);
+  _append_log(...arguments);
+}
+const _append_log = function () {
+  const log_el = document.createElement('div');
+  var logs = [];
+  for (var i in arguments) {
+    logs.push(arguments[i]);
+  }
+  log_el.innerText = logs.join(' ');
+  
+  var logEl = document.getElementById('log');
+  if (logEl != null) {
+    logEl.append(log_el);
+  }
+}
+window.onerror = function (message, source, lineno, colno, error) {
+  console.log("Error: " + message + "\n" + "Source: " + source + "\n" + "Line: " + lineno + "\n" + "Column: " + colno + "\n" + "Stack: " + error.stack);
+}
